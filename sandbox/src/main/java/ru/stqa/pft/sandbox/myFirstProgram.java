c5d16ab5c6e526addc1c819724b7118664ef0fe6
Evgeney Efremov <sir.axle@yandex.ru>
2018-11-21 20:26:06 +0300

    add classes square and rectangle

--- sandbox/src/main/java/ru/stqa/pft/sandbox/myFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/myFirstProgram.java
@@ -2,6 +2,16 @@ package ru.stqa.pft.sandbox;
 
 public class myFirstProgram {
   public static void main(String[] args) {
+    Square s = new Square(5);
+    System.out.println("Площадь квадрата со стороной "+ s.l + " = " + area(s));
+    Rectangle r = new Rectangle(4, 6);
+    System.out.println("Площадь прямоугольника со сторонами "+ r.a + " и " + r.b + " =  " + area(r));
     System.out.println("Hello,world!");
+  }
+  public static double area(Square s){
+    return s.l * s.l;
+  }
+  public static double area(Rectangle r){
+    return r.a * r.b;
   }
 }
