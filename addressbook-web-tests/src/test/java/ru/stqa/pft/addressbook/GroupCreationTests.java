fcc39361eed5413a37e189da93cb80341e4347ea
Evgeney Efremov <sir.axle@yandex.ru>
2018-11-26 23:17:42 +0300

    move common methods from GroupDeletionTests into TestBase and create new methods

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -1,14 +1,6 @@
 package ru.stqa.pft.addressbook;
 
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
-
-import java.util.concurrent.TimeUnit;
-
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.*;
 
 public class GroupCreationTests extends TestBase{
 
