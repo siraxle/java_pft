1a86cbbd701592bd936ab95659a1afb2fde05439
Evgeney Efremov <sir.axle@yandex.ru>
2018-11-27 19:20:35 +0300

    EditAddressBookTests are working now

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/AddNewData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/AddNewData.java
@@ -1,4 +1,55 @@
-package ru.stqa.pft.addressbook.model;
+package ru.stqa.pft.addressbook.tests;
 
 public class AddNewData {
+  private final String firstName;
+  private final String middleName;
+  private final String lastName;
+  private final String nickName;
+  private final String mobile;
+  private final String email;
+  private final String byear;
+  private final String notes;
+
+  public AddNewData(String firstName, String middleName, String lastName, String nickName, String mobile, String email, String byear, String notes) {
+    this.firstName = firstName;
+    this.middleName = middleName;
+    this.lastName = lastName;
+    this.nickName = nickName;
+    this.mobile = mobile;
+    this.email = email;
+    this.byear = byear;
+    this.notes = notes;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public String getMiddleName() {
+    return middleName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public String getNickName() {
+    return nickName;
+  }
+
+  public String getMobile() {
+    return mobile;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public String getByear() {
+    return byear;
+  }
+
+  public String getNotes() {
+    return notes;
+  }
 }
