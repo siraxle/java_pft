46b8d2409ce414d5e14faca55146aab02c18f8f7
Evgeney Efremov <sir.axle@yandex.ru>
2018-11-29 20:28:14 +0300

    add Equation into sandbox

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
@@ -2,10 +2,11 @@ package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class HelperBase {
-  protected ChromeDriver wd;
+  protected WebDriver wd;
 
   public HelperBase(ChromeDriver wd) {
     this.wd = wd;
