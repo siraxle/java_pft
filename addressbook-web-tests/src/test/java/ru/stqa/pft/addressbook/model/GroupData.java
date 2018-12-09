26af3d9c845391c5b0bbf35af0492b8a9988863c
Evgeney Efremov <sir.axle@yandex.ru>
2018-12-10 00:33:27 +0300

    add compare into group creation test

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -9,8 +9,8 @@ public class GroupData {
   private final String header;
   private final String footer;
 
-  public GroupData( String name, String header, String footer){
-    this.id = 0;
+  public GroupData(String name, String header, String footer){
+    this.id = Integer.MAX_VALUE;
     this.name = name;
     this.header = header;
     this.footer = footer;
@@ -44,25 +44,24 @@ public class GroupData {
   }
 
   @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    GroupData groupData = (GroupData) o;
+    return Objects.equals(name, groupData.name);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(name);
+  }
+
+  @Override
   public String toString() {
     return "GroupData{" +
             "id=" + id +
             ", name='" + name + '\'' +
             '}';
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    GroupData groupData = (GroupData) o;
-    return id == groupData.id &&
-            Objects.equals(name, groupData.name);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(id, name);
   }
 
 
