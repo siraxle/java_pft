79411f363cbb5ae1c8813b7ed62be32ddbeb83c7
Evgeney Efremov <sir.axle@yandex.ru>
2018-12-12 23:12:17 +0300

    add before/after Suite insted befor/after method in TesteBase

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 public class ContactCreationTests extends TestBase {
 
-  @Test
+  @Test (enabled = false)
   public void testAddContactToAddressBook(){
     app.getNavigationHelper().goToHomePage();
     List<ContactData> before = app.getContactHelper().getContactList();
