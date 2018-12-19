e95ae32980a5758c8ee042604cdcccc0c3f500e9
Evgeney Efremov <sir.axle@yandex.ru>
2018-12-19 20:21:45 +0300

    compare set in contact modification test

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -1,7 +1,11 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+
+import java.util.HashSet;
+import java.util.List;
 
 public class ContactModificationTests extends TestBase {
 
@@ -13,13 +17,19 @@ public class ContactModificationTests extends TestBase {
       app.getContactHelper().createContact(new ContactData("Евгений", "Ефремов", "Витальевич",
               "axle", "9522448961", "sir.axle@yandex.ru", "test1", "it is test"));
     }
-    app.getContactHelper().selectContact(0);
+    app.getContactHelper().returnToHomePage();
+    List<ContactData> before = app.getContactHelper().getContactList();
+    app.getContactHelper().selectContact(before.size()-1);
     app.getContactHelper().initContactModification();
-    app.getContactHelper().fillContactForm(new ContactData("first_name","middle_name",
+    ContactData contact = new ContactData("first_name","middle_name",
             "last_name","nick_name","12345","mail@mail.com",null,
-            "qwerty"), false);
+            "qwerty");
+    app.getContactHelper().fillContactForm(contact,false);
     app.getContactHelper().submitContactModification();
     app.getContactHelper().returnToHomePage();
+    List<ContactData> after = app.getContactHelper().getContactList();
+    Assert.assertEquals(after.size(), before.size());
+    Assert.assertEquals(new HashSet<Object>(before),new HashSet<Object>(after));
   }
 
 }
