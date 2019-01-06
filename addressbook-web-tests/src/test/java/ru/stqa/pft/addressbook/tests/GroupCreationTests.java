07602360176b7f2d47c7c8a4b634179202421766
Evgeney Efremov <sir.axle@yandex.ru>
2019-01-07 00:51:05 +0300

    add logback.xml

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -3,6 +3,8 @@ package ru.stqa.pft.addressbook.tests;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.thoughtworks.xstream.XStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
@@ -57,6 +59,7 @@ public class GroupCreationTests extends TestBase {
 
   @Test(dataProvider = "valideGroupsFromXml")//(enabled = false)
   public void testGroupCreation(GroupData group) {
+    logger.info("Start test GroupCreationTests");
     app.goTo().groupPage();
     Groups before = app.group().all();
     app.group().create(group);
@@ -65,6 +68,7 @@ public class GroupCreationTests extends TestBase {
     System.out.println(after.withAdded(group));
     group.withId(after.stream().mapToInt(g -> g.getId()).max().getAsInt());
     assertThat(after, equalTo(before.withAdded(group)));
+    logger.info("Stop test GroupCreationTests");
   }
 
   @Test(enabled = false)
