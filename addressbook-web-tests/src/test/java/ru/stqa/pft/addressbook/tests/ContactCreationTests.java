bfe1caebcce4e77e419e21f76d30b32a6f3d5413
Evgeney Efremov <sir.axle@yandex.ru>
2019-01-10 14:48:24 +0300

    add valid contacts in creat contact test (it workgit add .)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -18,21 +18,20 @@ public class ContactCreationTests extends TestBase {
     @DataProvider
     public Iterator<Object[]> validContacts() throws IOException {
         List<Object[]> list = new ArrayList<Object[]>();
-        //File photo1 = new File("src/test/resources/file.jpg");
-        //File photo2 = new File("src/test/resources/monkey.png");
-        try (BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.csv")))){
-            String line = reader.readLine();
-            while (line != null){
-                String[] split = line.split(";");
-                list.add(new Object[]{new ContactData().withFirstName(split[0]).withLastName(split[1]).withMiddleName(split[2])
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.csv")));
+        String line = reader.readLine();
+        while (line != null) {
+            String[] split = line.split(";");
+            list.add(new Object[]{new ContactData().withFirstName(split[0]).withLastName(split[1]).withMiddleName(split[2])
                         .withNickName(split[3]).withEmail(split[4]).withGroup(split[5]).withNotes(split[6])
                         .withHomePhone(split[7]).withMobilePhone(split[8]).withWorkPhone(split[9])
                         .withPhoto(new File(split[10]))});
-            }
-            return list.iterator();
+            line = reader.readLine();
+
         }
+        return list.iterator();
     }
-    
+
 
     @Test(dataProvider = "validContacts")//(enabled = false)
     public void testContactCreation(ContactData contact) {
